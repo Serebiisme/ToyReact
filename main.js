e0bb86997696aaaf05ec4fd5786af213c97ad1ab
wangxiyang_wang <wangxiyang_wang@youzan.com>
2021-03-16 22:10:28 +0800

    Fix: UPDAET

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-// const c = <div></div>
 
-// export default c
-console.log(2)
+function createElement(tagName, attrs, ...children) {
+    const e = document.createElement(tagName)
+    Object.entries(Object(attrs || {})).forEach(([key, value]) => {
+        e.setAttribute(key, value)
+    })
+    children.forEach(child => {
+        e.appendChild(child)
+    })
+    return e
+}
+
+
+const app = <div>
+    <div></div>
+    <div></div>
+    <p></p>
+</div>
+
+window.app = app
