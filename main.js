cb0675ad8815d27213bab47d4719ae6398aac6a1
wangxiyang_wang <wangxiyang_wang@youzan.com>
2021-03-23 22:53:03 +0800

    Feat: 完成基于range的DOM绘制

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 import { createElement, render, Component } from "./toy-react";
 class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      a: 11111122223333,
+      b: 2,
+    };
+  }
+
   render() {
-    return <div>
-      <h1>my Component</h1>
-      {this.props.children}
-    </div>;
+    return (
+      <div>
+        <h1>my Component</h1>
+        <p>{this.state.a}</p>
+        {this.props.children}
+      </div>
+    );
+    // return null
   }
 }
 
@@ -14,7 +27,7 @@ const app = (
     <div>321</div>
     <p>
       <span>222</span>
-      <span>3233</span>
+      <span>232342342</span>
     </p>
   </MyComponent>
 );
--- toy-react.js
+++ toy-react.js
@@ -1,77 +1,135 @@
 export function createElement(type, attrs, ...children) {
   let e;
-  if (typeof type === 'string') {
-    e = new ElementWrapper(type)
+  // 判断是否为 「 原生标签 」
+  if (typeof type === "string") {
+    e = new ElementWrapper(type);
+    // 设置属性
+    Object.entries(Object(attrs || {})).forEach(([key, value]) => {
+      e.setAttribute(key, value);
+    });
   } else {
-    e = new type()
+    e = new type(attrs);
   }
 
-  Object.entries(Object(attrs || {})).forEach(([key, value]) => {
-    e.setAttribute(key, value)
-  })
-  children.forEach(child => {
-    console.log(child)
-    if (typeof child === 'string') {
-      child = new TextWrapper(child)
+  // 添加子元素
+  // 这里的 child 实际是指
+  children.forEach((child) => {
+    // 文本标签
+    if (typeof child === "string") {
+      child = new TextWrapper(child);
     }
+    // 多子节点
     if (Array.isArray(child)) {
       for (const c of child) {
-        e.appendChild(c)
+        e.appendChild(c);
       }
+      // 单子节点
     } else {
-      e.appendChild(child)
+      e.appendChild(child);
     }
-  })
-  return e
+  });
+  return e;
 }
 
+const RENDER_TO_DOM = Symbol("render to dom");
+
+// 原生标签组件
 class ElementWrapper {
   constructor(type) {
-    this.root = document.createElement(type)
-
+    this.root = document.createElement(type);
   }
 
   setAttribute(name, value) {
-    this.root.setAttribute(name, value)
+    this.root.setAttribute(name, value);
   }
 
   appendChild(component) {
-    this.root.appendChild(component.root)
+    // 当 state值作为 child 传入时, 需判断子类型
+    if (typeof component !== "object") {
+      component = new TextWrapper(component);
+    }
+    // this.root.appendChild(component.root);
+
+    let range = new Range();
+    // 放置末尾
+    range.setStart(this.root, this.root.childNodes.length);
+    range.setEnd(this.root, this.root.childNodes.length)
+
+    component[RENDER_TO_DOM](range)
+  }
+
+  [RENDER_TO_DOM](range) {
+    // 删除范围内的内容
+    range.deleteContents();
+    // 插入节点
+    range.insertNode(this.root);
   }
 }
 
+// 文本标签组件
 class TextWrapper {
   constructor(content) {
-    this.root = document.createTextNode(content)
+    this.root = document.createTextNode(content);
+  }
+
+  [RENDER_TO_DOM](range) {
+    // 删除范围内的内容
+    range.deleteContents();
+    // 插入节点
+    range.insertNode(this.root);
   }
 }
+
 
 export class Component {
   constructor() {
-    this.props = Object.create(null)
-    this.props.children = []
+    this.props = Object.create(null);
+    this.props.children = [];
     this._root = null;
   }
 
   setAttribute(name, value) {
-    this.props[name] = value
-
+    this.props[name] = value;
   }
 
   appendChild(component) {
-    this.props.children.push(component)
+    this.props.children.push(component);
   }
 
-  get root() {
-    if (!this._root) {
-      // 递归操作
-      this._root = this.render().root
+  // 根据位置信息去 render DOM
+  [RENDER_TO_DOM](range) {
+    // 递归
+    const component = this.render()
+    if (component) {
+      component[RENDER_TO_DOM](range);
     }
+  }
 
-    return this._root
+  // 从取单个元素 转变为 取一个范围
+  // get root() {
+  //   if (!this._root) {
+  //     const nodes = this.render()
+  //     // 递归操作
+  //     this._root = nodes ? nodes.root : null
+  //   }
+
+  //   return this._root
+  // }
+
+  // ! 子组件需要实现 render 方法
+  render() {
+    throw new Error('render function should be rewrite !')
   }
 }
 
 export function render(component, parentElement) {
-  parentElement.appendChild(component.root)
+  let range = new Range();
+  // 圈定 父元素 的内容
+  range.setStart(parentElement, 0);
+  // ! 这里不能用 children，而是用 childrenNodes 是因为要涵盖 注释节点 和 文本节点
+  range.setEnd(parentElement, parentElement.childNodes.length);
+  // 删除range 里的内容
+  range.deleteContents();
+  // 重新把内容渲染进去
+  component[RENDER_TO_DOM](range);
 }
